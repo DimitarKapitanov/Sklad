aa08e2b175e8c8d3b0e83fe80cb22eed910cae76
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2023-11-28 09:01:21 +0200

    Add table sorting

--- client-app/src/features/products/dashboard/ProductTable.tsx
+++ client-app/src/features/products/dashboard/ProductTable.tsx
@@ -1,29 +1,88 @@
+import { MouseEventHandler, useCallback, useState } from "react";
 import { Product } from "../../../app/models/product";
 import { Button, Icon, Table } from "semantic-ui-react";
 
 interface Props {
-    tableHeader: string[];
     products: Product[];
     selectProduct: (id: string) => void;
     deleteProduct: (id: string) => void;
 }
 
-export default function ProductTable({ products, tableHeader, selectProduct, deleteProduct }: Props) {
+type Data = Product[];
+type SortKeys = keyof Data[0];
+type SortOrder = "asc" | "desc";
 
-    tableHeader = ["Име", "Количество", "Доставна цена", "Продажна цена", "Категория", "Мярка", "Описание", "Edit/Delete"];
+function sortData({
+    tableData,
+    sortKey,
+    reverse
+}: {
+    tableData: Data;
+    sortKey: SortKeys;
+    reverse: boolean;
+}) {
+    if (!sortKey) return tableData;
+
+    const sortedData = tableData.sort((a, b) => {
+        return a[sortKey] > b[sortKey] ? 1 : -1;
+    });
+
+    if (reverse) return sortedData.reverse();
+    return sortedData
+}
+
+function SortButton({
+    sortOrder, columnKey, sortKey, onClick} : {
+        sortOrder: SortOrder;
+        columnKey: SortKeys;
+        sortKey: SortKeys;
+        onClick: MouseEventHandler<HTMLButtonElement>;
+    }) {
+        return <Button onClick={onClick} className={`${sortKey === columnKey && sortOrder === 'desc' ? "sort-button sort-reverse" : "sort-button"}`}>
+            <Icon name="caret up"/>
+        </Button>
+    }
+
+export default function ProductTable({ products, selectProduct, deleteProduct, }: Props) {
+
+    const [sortKey, setSortKey] = useState<SortKeys>('isDeleted');
+    const [sortOrder, setSortOrder] = useState<SortOrder>('asc');
+
+    const tableHeader: {key: SortKeys, label: string}[] = [
+        {key: "name", label: "Име"}, 
+        {key: "quantity", label: "Количество"},
+        {key: "deliveryPrice", label: "Доставна цена"},
+        {key:"price", label:"Продажна цена"},
+        {key: "category", label:"Категория"}, 
+        {key: "unitAcronym", label:"Мярка"},
+        {key: "description",label:"Описание"}, 
+        {key: "isDeleted", label:"Edit/Delete"}];
+
+    const sortedData = useCallback(() => sortData({ tableData: products, sortKey, reverse: sortOrder === 'desc' }), [products, sortKey, sortOrder]);
+
+    function changeSort(key: SortKeys): void {
+        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+
+        setSortKey(key);
+    }
 
     return (
         <Table celled padded selectable>
             <Table.Header>
                 <Table.Row>
-                    {tableHeader.map((head, i) => (
-                        <Table.HeaderCell key={i}>{head}</Table.HeaderCell>
+                    {tableHeader.map((row) => (
+                        <Table.HeaderCell key={row.key}>{row.label} <SortButton
+                         columnKey={row.key}
+                         onClick={() => changeSort(row.key)}
+                         {...{sortOrder, sortKey}}
+                         />
+                         </Table.HeaderCell>
                     ))}
                 </Table.Row>
             </Table.Header>
 
             <Table.Body>
-                {products.map(product => (
+                {sortedData().map((product) => (
                     <Table.Row key={product.id}>
                         <Table.Cell>{product.name}</Table.Cell>
                         <Table.Cell>{product.quantity}</Table.Cell>
